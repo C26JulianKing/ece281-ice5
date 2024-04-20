c82cee36651c5b027e0eb1b624fd0e83c35ec1b6
C26JulianKing <github noreply email@users.noreply.github.com>
2024-04-19 21:12:02 -0600

    Syntax Fix

--- src/hdl/elevator_controller_fsm.vhd
+++ src/hdl/elevator_controller_fsm.vhd
@@ -100,19 +100,19 @@ begin
                 s_floor2 when(f_Q = s_floor1 and i_up_down = '1') else
                 s_floor3 when(f_Q = s_floor2 and i_up_down = '1') else
                 s_floor4 when(f_Q = s_floor3 and i_up_down = '1') else
-                s_floor4 when(f_Q = s_floor4 and i_up_down = '0') else
+                s_floor4 when(f_Q = s_floor4 and i_up_down = '1') else
                 -- going down
                 s_floor3 when(f_Q = s_floor4 and i_up_down = '0') else
                 s_floor2 when(f_Q = s_floor3 and i_up_down = '0') else
                 s_floor1 when(f_Q = s_floor2 and i_up_down = '0') else
-                s_floor2; -- default case
+                f_Q; -- default case
   
 	-- Output logic
-    with f_Q select
-        o_floor <= "0001" when s_floor1,
-                   "0011" when s_floor3,
-                   "0100" when s_floor4,
-                "0010" when others; -- default is floor 2
+        o_floor <= "0001" when f_Q = s_floor1 else
+                   "0010" when f_Q = s_floor2 else
+                   "0011" when f_Q = s_floor3 else
+                   "0100" when f_Q = s_floor4 else
+                   "0010"; -- default is floor 2
 	
 	-------------------------------------------------------------------------------------------------------
 	
@@ -122,12 +122,12 @@ begin
     begin
          -- synchronous reset
         if rising_edge(i_clk) then
-            if i_reset <= '1' then
+            if i_reset = '1' then
                 f_Q <= s_floor2;
-            elsif i_stop <= '0' then
+            elsif i_stop = '0' then
                 f_Q <= f_Q_next;
-            else
-                f_Q <= f_Q;
+           -- else
+              --  f_Q <= f_Q;
             end if;
        end if;
         -- if elevator is enabled, advance floors
